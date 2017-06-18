277e1dce95dccd09fe3ece6682f906fc363a7da5
Juliana Hosoume <ju.hosoume@gmail.com>
2017-06-18 00:27:47 -0300

    User logic created

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -1,10 +1,52 @@
 package controllers
 
 import play.api.mvc._
+import models.User
+import models.UserDAO
+import javax.inject.Inject
+import play.api.data._
+import play.api.data.Forms._
+import javax.inject.Singleton
+import play.api.i18n.I18nSupport
+import play.api.i18n.MessagesApi
 
-class UsuarioController extends Controller {
+@Singleton
+class UserController @Inject()(dao: UserDAO, val messagesApi: MessagesApi) extends Controller with I18nSupport {
   
   def login(email: String, password:String) = Action {
     Ok("foo")
   }
+
+  def show(id: Long) = TODO
+
+  def list = Action {
+    var users = dao.list
+    Ok(views.html.users.listing(users))
+  }
+
+  def newUser = Action {
+    Ok(views.html.users.newUser(userForm))
+  }
+
+  def newUserSubmission = Action { implicit request =>
+    userForm.bindFromRequest.fold(
+      formWithErrors => {
+        BadRequest(views.html.users.newUser(formWithErrors))
+      },
+      user => {
+        val newUser = User(0, user.email, user.password, false)
+        dao.save(newUser)
+        var users = dao.list
+        Created(views.html.users.listing(users))
+      }
+  )}
+
+  val userForm = Form(
+    mapping(
+      "Email" -> nonEmptyText,
+      "Password" -> nonEmptyText
+    )(UserVO.apply)(UserVO.unapply)
+  )
 }
+
+case class UserVO(email: String, password: String)
--- app/models/User.scala
+++ app/models/User.scala
@@ -1,12 +1,35 @@
 package models
 
-case class User(email: String, password: String)
+import anorm.SQL
+import anorm.SqlQuery
+import anorm.RowParser
+import anorm.Macro
+import anorm.SqlStringInterpolation
+import anorm.SqlParser
+import play.api.db.DB
+import play.api.Play.current
+import javax.inject.Inject
+import play.api.db.Database
+import javax.inject.Singleton
 
-object User {
-  var products = Set(
-        User("foo", "foo"), 
-        User("blah", "blah")
-      )
-      
-   def searchByEmail(email : String) = products.filter(u => u.email == email).toList(0)  
+case class User(id: Int, email: String, password: String, admin: Boolean)
+
+class UserDAO @Inject() (database: Database) {
+  val parser : RowParser[models.User] = Macro.namedParser[models.User]
+
+  def save(user: User) = database.withConnection { implicit connection =>
+    val id: Option[Long] = SQL(
+      """INSERT INTO TB_USER(EMAIL, PASSWORD, ADMIN)
+          values ({email}, {password}, {admin})""")
+     .on('email -> user.email,
+         'password -> user.password,
+         'admin -> user.admin).executeInsert()
+  }
+
+  def list = database.withConnection { implicit connection =>
+    SQL"SELECT ID, EMAIL, PASSWORD, ADMIN FROM TB_USER".as(parser.*)
+  }
+
+
+  //def searchByEmail(email : String) = products.filter(u => u.email == email).toList(0)  
 }
